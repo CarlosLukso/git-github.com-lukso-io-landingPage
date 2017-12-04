3cc985398373178f493a340ea768a55a000e8301
Carlos Bonilla <carlos@kweak.ly>
2017-12-04 21:36:08 +0100

    Lukso index

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109039231-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-109039231-1');
+    </script>
+
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>Lukso</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+    <!-- CSS Files -->
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./assets/css/now-ui-kit.css?v=1.1.0" rel="stylesheet" />
+    <!-- CSS Just for demo purpose, don't include it in your project -->
+    <link href="./assets/css/demo.css" rel="stylesheet" />
+    <link href="./assets/css/mycss.css" rel="stylesheet" />
+    <script src="./assets/js/anime.js"></script>
+   
+
+
+<style type="text/css">
+    body{
+        background-color: #ea5874;
+    }
+    #mc_embed_signup{
+        background:none; 
+        clear:left; 
+        font:14px 'Montserrat', 'Helvetica Neue',  Arial, sans-serif;; 
+        width:415px;
+        text-align: center;
+        margin: 0 28%;
+    }
+    /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+       We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+    #mc_embed_signup .email {
+        float: left;
+        margin: 0;
+        background: no-repeat;
+        color: white !important;
+        border: solid thin;
+        padding: 8px 8px 8px 8px;
+        border-radius: 5px 0 0 5px;
+        width: 260px;
+    }
+
+   #mc_embed_signup [type=reset], [type=submit], button, html [type=button] {
+        padding: 8px 13px 8px 12px;
+        background: white;
+        border: solid thin white !important;
+        border-radius: 0 5px 5px 0;
+        margin:0 0 0 -2px;
+        color: #ea5874;
+    }
+    @media only screen 
+    and (min-device-width : 375px) 
+    and (max-device-width : 667px) 
+    and (orientation : portrait) { 
+        #mc_embed_signup{
+            clear:left; 
+            font:14px 'Montserrat', 'Helvetica Neue',  Arial, sans-serif;
+            width: 100%;
+            /*max-width:240px;*/
+            text-align: center;
+            padding: 0;
+            margin:0;
+        }
+        /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+           We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+        #mc_embed_signup .email {
+            margin: 5px 0;
+            float: inherit;
+            background: no-repeat;
+            color: white !important;
+            border: solid thin;
+            padding: 8px 8px 8px 8px;
+            border-radius: 5px;
+            width: 100%;
+            max-width: 250px;
+        }
+
+       #mc_embed_signup [type=reset], [type=submit], button, html [type=button] {
+            padding: 8px 7px 8px 7px;;
+            background: white;
+            border: solid thin white !important;
+            border-radius: 5px;
+            margin: 5px 0;
+            width: 100%;
+            max-width: 250px;
+        }
+    }
+    ::-webkit-input-placeholder { /* Chrome/Opera/Safari */
+    color: white;
+    }
+    ::-moz-placeholder { /* Firefox 19+ */
+      color: white;
+    }
+    :-ms-input-placeholder { /* IE 10+ */
+      color: white;
+    }
+    :-moz-placeholder { /* Firefox 18- */
+      color: white;
+    }
+</style>
+</head>
+
+<body class="index-page sidebar-collapse">
+    <div class="wrapper">
+        <div id="top" class="page-header clear-filter canvas-interactive-wrapper">
+            <canvas id="canvas-basic" ></canvas>
+            <div id="cssSelector" class="container"> 
+                <div class="content-center brand">
+                    <img class="n-logo" src="./assets/img/lukso-logo.svg" alt=""><br>
+                    <!-- Begin MailChimp Signup Form -->
+
+
+            <div id="mc_embed_signup">
+            <form action="https://lukso.us16.list-manage.com/subscribe/post?u=7ed90a2675919bb4a24bd1d07&amp;id=308022dbb4" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll">
+                
+                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email address" required style="color:white;">
+                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_7ed90a2675919bb4a24bd1d07_308022dbb4" tabindex="-1" value=""></div>
+                <div class="clear"><input type="submit" value="Keep me updated" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                </div>
+            </form>
+            </div>
+
+<!--End mc_embed_signup-->
+                    <!--<a href="#about" class="page-scroll btn-phanton btn btn-xl smoothScroll">Keep me in the loop</a>-->
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<!--   Core JS Files   -->
+<script src="./assets/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
+<script src="./assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="./assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+<!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+<script src="./assets/js/plugins/bootstrap-switch.js"></script>
+<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+<script src="./assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+<!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+<script src="./assets/js/plugins/bootstrap-datepicker.js" type="text/javascript"></script>
+<!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
+<script src="./assets/js/now-ui-kit.js?v=1.1.0" type="text/javascript"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        // the body of this function is in assets/js/now-ui-kit.js
+        nowuiKit.initSliders();
+    });
+
+    function scrollToDownload() {
+
+        if ($('.section-download').length != 0) {
+            $("html, body").animate({
+                scrollTop: $('.section-download').offset().top
+            }, 1000);
+        }
+    }
+</script>
+<script src="assets/js/granim.min.js"></script>
+<script>
+        var granimInstance = new Granim({
+            element: '#canvas-basic',
+            name: 'basic-gradient',
+            direction: 'left-right',
+            opacity: [1, 1],
+            isPausedWhenNotInView: true,
+            states : {
+                "default-state": {
+                    gradients: [
+                        ['#fffbe5', '#eb5774'],
+                        ['#eb5774', '#f8afb5'],
+                        ['#f8afb5', '#eb5774']
+                    ]
+                }
+            }
+        });
+    </script>
+ <!--<script>
+        var cssSelector = anime({
+          targets: '#cssSelector .brand',
+          top: {
+            value: [350, 250],
+            easing: 'easeOutCubic',
+            duration: 800,
+          },
+          opacity: {
+            value: [0, 1],
+            duration: 2800,
+            easing: 'easeOutCubic'
+          }
+        });
+    </script>-->
+</html> 
